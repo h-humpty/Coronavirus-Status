aba4bd8e9337b54a38eeeda61e896a8bea4ece2a
passionatedeveloper223 <strongwolf223@gmail.com>
2022-01-03 19:29:58 +0600

    Adding cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,39 @@
 import react from 'react';
-// : { confirmed, recovered, deaths, lastUpdate } 
-const Cards = ({data:{confirmed,recovered,deaths,lastUpdate}}) => {
-    console.log(confirmed)
+import styles from './Cards.module.css';
+import { Typography, Grid } from '@material-ui/core';
+import CardComponent from './Card/Card';
+
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+    if (!confirmed) {
+        return 'Loading...';
+    }
     return (
-        <h1>thisi Cards</h1>
+        <div className={styles.container}>
+            <Typography gutterBottom variant="h4" component="h2">Global</Typography>
+            <Grid container spacing={3} justifyContent="center">
+                <CardComponent
+                    className={styles.infected}
+                    cardTitle="Infected"
+                    value={confirmed.value}
+                    lastUpdate={lastUpdate}
+                    cardSubtitle="Number of active cases from COVID-19."
+                />
+                <CardComponent
+                    className={styles.recovered}
+                    cardTitle="Recovered"
+                    value={recovered.value}
+                    lastUpdate={lastUpdate}
+                    cardSubtitle="Number of recoveries from COVID-19."
+                />
+                <CardComponent
+                    className={styles.deaths}
+                    cardTitle="Deaths"
+                    value={deaths.value}
+                    lastUpdate={lastUpdate}
+                    cardSubtitle="Number of deaths caused by COVID-19."
+                />
+            </Grid>
+        </div>
     )
 }
 export default Cards;
